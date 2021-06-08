39e9d22cd3a79715035a522d9b3c8d82158138df
Zhanara Zhorobaeva <janashj@bastion-fuchicorp-com.us-central1-b.c.geometric-sled-282503.internal>
2021-06-08 01:37:26 +0000

    another commit

--- deployments/docker/app.py
+++ deployments/docker/app.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return jsonify({
-        'message': 'hello world',
+        'message': 'hello, I am devops, nice to meet you, happy debugging after school',
         'environment': os.environ.get('ENVIRONMENT'),
+        'owner': 'janashj',
         'namespace': os.environ.get('NAMESPACE')
     })
 @app.route('/soon')
@@ -14,6 +15,11 @@ def comming_soon():
     return jsonify({
         'message': 'This is comming soon page!!'
     })
+
+@app.route('/janarajorobaeva')
+def comming_soon():
+    return jsonify({
+        'message': 'This is Janara Jorobaeva, welcome a board!!'
+    })
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
-
